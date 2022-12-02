6f70f5038e52eeed9c8cc00b501a00742ec5609f
Ayresia <nalizyoncatania0@gmail.com>
2022-12-02 19:42:16 +0100

    feat: logic changes and base url change

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use serde::Deserialize;
 use serde_repr::Deserialize_repr;
 
-const BASE_URL: &str = "https://ayresiacgm.herokuapp.com";
+const BASE_URL: &str = "https://t1.ayresia.dev";
 
 #[derive(Deserialize)]
 pub struct EntriesResponse {
     sgv: f32,
-    trend: TrendDirection
+    trend: TrendDirection,
 }
 
 #[derive(Deserialize_repr, Debug)]
@@ -21,34 +21,44 @@ pub enum TrendDirection {
     SingleDown = 6,
     DoubleDown = 7,
     NotComputable = 8,
-    OutOfRange = 9
-}
-
-#[tokio::main]
-pub async fn main() {
-    if let Ok(res) = reqwest::get(format!("{BASE_URL}/api/v1/entries.json?count=2")).await {
-        if let Ok(encoded_response) = res.json::<[EntriesResponse; 2]>().await {
-            let converted_value = encoded_response[0].sgv / 18_f32;
-            let delta = (encoded_response[0].sgv - encoded_response[1].sgv) / 18_f32;
-            let trend = parse_trend(&encoded_response[0].trend);
-
-            println!("{converted_value:.1} {delta:+.1} {trend}");
-            return;
-        }
-    }
-
-    println!("Unable to fetch glucose");
+    OutOfRange = 9,
 }
 
 pub fn parse_trend(trend: &TrendDirection) -> &str {
-    return match trend {
+    match trend {
         TrendDirection::DoubleUp => "↑↑",
-        TrendDirection::SingleUp  => "↑",
+        TrendDirection::SingleUp => "↑",
         TrendDirection::FortyFiveUp => "↗︎",
         TrendDirection::Flat => "→",
         TrendDirection::FortyFiveDown => "↘",
         TrendDirection::SingleDown => "↓",
         TrendDirection::DoubleDown => "↓↓",
-        _ => ""
+        TrendDirection::NotComputable => "-",
+        _ => "",
     }
+}
+
+#[tokio::main]
+pub async fn main() {
+    let result = match reqwest::get(format!("{BASE_URL}/api/v1/entries.json?count=2")).await {
+        Ok(res) => res,
+        Err(_) => {
+            eprintln!("Unable to fetch glucose");
+            return;
+        }
+    };
+
+    let encoded_response = match result.json::<[EntriesResponse; 2]>().await {
+        Ok(response) => response,
+        Err(_) => {
+            eprintln!("Unable to transform response to json");
+            return;
+        }
+    };
+
+    let converted_value = encoded_response[0].sgv / 18_f32;
+    let delta = (encoded_response[0].sgv - encoded_response[1].sgv) / 18_f32;
+    let trend = parse_trend(&encoded_response[0].trend);
+
+    println!("{converted_value:.1} {delta:+.1} {trend}");
 }
